6354b18f0479dfe8bbfdff369288f34e1d1bb4a3
Ruslan Frolov <r.frolov@1440.space>
2023-10-10 15:13:55 +0300

    refactor: remove commented code

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -3,20 +3,5 @@ package ru.practicum.shareit.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.Collection;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-//    User createUser(User user);
-//
-//    User updateUser(User userToUpdate);
-//
-//    User getUserById(Long userId);
-//
-//    User deleteById(Long id);
-//
-//    Collection<User> getAllUsers();
-//
-//    boolean isUserExists(long userId);
-//
-//    boolean isEmailUnique(String email, Long userId);
 }
