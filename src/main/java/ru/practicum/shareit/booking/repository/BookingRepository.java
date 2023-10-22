066b36065ccc64ded13aebe2afd292307bcc8356
Ruslan Frolov <r.frolov@1440.space>
2023-10-22 14:15:15 +0300

    chore: remove commented code

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -9,26 +9,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-//    @Query(" select i from Item i " +
-//            "where upper(i.name) like upper(concat('%', ?1, '%')) " +
-//            " or upper(i.description) like upper(concat('%', ?1, '%'))")
-//    List<Booking> search(String text);
-
     @Query("select b " +
             "from Booking as b " +
             "where b.bookerId = ?1 " +
             "order by b.startDate desc")
     List<Booking> findAllBookingsForBookerByStatus(Long bookerId);
-
-//    @Query("select i " +
-//            "from Item as i " +
-//            "where i.ownerId = ?1 " +
-//            ";" +
-//            "select b " +
-//            "from Booking as b " +
-//            "where b.itemId = ?1 " +
-//            "order by b.startDate desc")
-//    List<Booking> findAllBookingsForItemsOwner(Long ownerId);
 
     List<Booking> findByBookerIdAndEndDateIsBefore(Long bookerId, LocalDateTime endDate, Sort sort);
 
